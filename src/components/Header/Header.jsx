1536604675cd2054cc3cae63ef36b064a282437a
DevTabibur <tobiburrohman2@gmail.com>
2022-03-31 01:23:04 +0600

    header done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,46 @@
-import React from 'react';
-import './header.css'
+import React from "react";
+import "./header.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Button,
+  Container,
+  Form,
+  FormControl,
+  Nav,
+  Navbar,
+  NavDropdown,
+} from "react-bootstrap";
 
 const Header = () => {
-    return (
-        <div>
-            <h1>Header</h1>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Meal Restaurant</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mx-auto">
+              <Nav.Link href="#home">HOME</Nav.Link>
+              <Nav.Link href="#meals">MEALS</Nav.Link>
+              <Nav.Link href="#about">ABOUT</Nav.Link>
+              <Nav.Link href="#contact">CONTACT</Nav.Link>
+            </Nav>
+            <Nav>
+              <Form className="d-flex">
+                <FormControl
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 
 export default Header;
