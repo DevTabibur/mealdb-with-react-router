d297125f8bb74b84ffc66abd32cc8a58d32c918e
DevTabibur <tobiburrohman2@gmail.com>
2022-03-31 01:15:32 +0600

    all compo done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <>
-      <h1 className='text-danger'>Hello world!</h1>
+      <Header/>
     </>
   );
 }
