f51f006924dc2a7e3088fa624f151db17e377cc0
DevTabibur <tobiburrohman2@gmail.com>
2022-03-31 13:20:10 +0600

    homepage link updated

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Home.css'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
         <div className="container-fluid">
             <div className="row">
                 <div className="col-md-4"></div>
-                <div className="col-md-4 text-center mx-auto">
+                <div className="col-md-4 text-center py-5 mt-5 mx-auto">
+                    <div className="inner-section">
                     <h2>Welcome to Meal Restaurant</h2>
-                    <button>Explore Meals</button>
+                    <Link to="/restaurants">Explore Meals</Link>
+                    </div>
                 </div>
                 <div className="col-md-4"></div>
             </div>
