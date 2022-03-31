6db84593f852ecf53eb6bf8aa2154b58da18111a
DevTabibur <tobiburrohman2@gmail.com>
2022-03-31 13:46:19 +0600

    error page done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import MealDetails from './components/MealDetails/MealDetails';
+import NotFound from './components/NotFound/NotFound';
 import Restaurant from './components/Restaurant/Restaurant';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home/>}></Route>
         <Route path="/restaurants" element={<Restaurant/>}></Route>
         <Route path="/meal-details/:id" element={<MealDetails/>}></Route>
+        <Route path='*' element={<NotFound/>}></Route>
       </Routes>
       <Footer/>
 
---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,28 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import Image from "../../Images/404.jpg";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+  return (
+    <div className="container py-5 my-4">
+      <div className="row">
+        <div className="col-md-6 order-2">
+          <div className="inner-section d-flex align-items-center justify-content-center">
+            <Link to="/" className=" mb-0">Go Back to Home</Link>
+            <Link to="/contact" className="mt-0">Contact Us</Link>
+          </div>
+        </div>
+
+        <div className="col-md-6">
+          <div className="error-img-div w-100 order-1">
+            <img className="error-img w-100" src={Image} alt="error__img" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
